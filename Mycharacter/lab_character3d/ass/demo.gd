31113f6ecbc6a88f8816519a14ab8dc7f7b77c8b
Thanaphon <thanaphon.phap@kkumail.com>
2025-09-19 21:15:58 +0700

    5

--- Mycharacter/lab_character3d/ass/demo.gd
+++ Mycharacter/lab_character3d/ass/demo.gd
@@ -3,11 +3,10 @@ extends Node3D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	$Walking/AnimationPlayer.play("MeleeLib/Die1")
-	$Walking2/AnimationPlayer.play("MeleeLib/HeavyRunning")
-	$Walking3/AnimationPlayer.play("MeleeLib/HeavyWalking")
-	$Walking4/AnimationPlayer.play("MeleeLib/root-Roll")
-	$Praying/AnimationPlayer.play("MeleeLib/Heavy1")
+	$Praying/AnimationPlayer.play("m")
+	$Praying2/AnimationPlayer.play("MeleeLib/Die1")
+	$Praying3/AnimationPlayer.play("MeleeLib/HeavyRunning")
+	$Praying4/AnimationPlayer.play("MeleeLib/HeavyWalking")
 	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
